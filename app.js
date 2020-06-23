c63d5f6b5d4de263a76457a839ea5c168b67f2eb
Suyeon Kang <tndusrkd92@gmail.com>
2020-06-23 22:15:00 +0900

    Insert current weather data into html

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 'use strict'
 
+/* DOM STRINGS */
 const DOMstring = {
     tempDescription: document.querySelector('.temperature__description'),
-    tempDegree:  document.querySelector('.temperature__degree'),
+    tempDegree: document.querySelector('.temperature__degree'),
     timezone: document.querySelector('.location__timezone'),
     icon: document.querySelector('.icon'),
     wind: document.querySelector('.weather-info__wind'),
@@ -12,55 +13,66 @@ const DOMstring = {
 };
 
 class Weather {
-
     getLocalTime(timestamp) {
         let sec = timestamp;
         let date = new Date(sec * 1000);
-        let timestr = date.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+        let timestr = date.toLocaleTimeString([], {
+            hour: '2-digit',
+            minute: '2-digit'
+        });
         return timestr;
     }
 
     async getData(lat, lon) {
-            try {
-                const api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=941bf83d26c6b4087d00fe58d945f23e`;
-                let res = await (await fetch(api)).json();
-                this.location = `${res.name}, ${res.sys.country }`;
-                this.description = res.weather[0].description;
-                this.icon = `http://openweathermap.org/img/wn/${res.weather[0].icon}@2x.png`;
-                this.degree = res.main.temp;
-                this.wind = res.wind.speed;
-                this.humidity = res.main.humidity;
-                this.sunrise = this.getLocalTime(res.sys.sunrise);
-                this.sunset = this.getLocalTime(res.sys.sunset);
-                console.log(res);
-            
-            } catch (error) {
-                console.log(error);
-            }
+        try {
+            const api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=941bf83d26c6b4087d00fe58d945f23e`;
+            let res = await (await fetch(api)).json();
+
+            this.location = `${res.name}, ${res.sys.country}`;
+            this.description = res.weather[0].description;
+            this.icon = `http://openweathermap.org/img/wn/${res.weather[0].icon}@2x.png`;
+            this.degree = res.main.temp;
+            this.wind = res.wind.speed;
+            this.humidity = res.main.humidity;
+            this.sunrise = this.getLocalTime(res.sys.sunrise);
+            this.sunset = this.getLocalTime(res.sys.sunset);
+            console.log(res);
+        } catch (error) {
+            console.log(error);
+        }
     }
 
-    renderWeather() {
-        console.log(this.wind);
-    } 
-}; 
+    renderData() {
+        DOMstring.tempDegree.textContent = this.degree;
+        DOMstring.tempDescription.textContent = this.description;
+        DOMstring.timezone.textContent = this.location;
+        DOMstring.icon.src = this.icon;
+        DOMstring.wind.textContent = `${this.wind} m/s`;
+        DOMstring.sunrise.textContent = this.sunrise;
+        DOMstring.sunset.textContent = this.sunset;
+        DOMstring.humidity.textContent = `${this.humidity}%`;
+    }
+};
 
-const a = new Weather();
-a.renderWeather();
 
 /* Event Handler */
 window.addEventListener('load', () => {
-
     if (navigator.geolocation) {
         const weather = new Weather();
 
-        navigator.geolocation.getCurrentPosition(position =>  {
+        navigator.geolocation.getCurrentPosition(async (position) => {
             const lon = position.coords.longitude;
             const lat = position.coords.latitude;
 
-            // Get data from API
-            weather.getData(lat,lon);
+            try {
+                // GET DATA FROM API
+                await weather.getData(lat, lon);
+
+                // INSERT WEATHER DATA
+                weather.renderData();
+            } catch (error) {
+                console.log(error)
+            }
         });
-    }
-}); 
-
-
+    };
+});
