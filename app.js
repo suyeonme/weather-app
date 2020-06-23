592a9d311c17794ba34cb94ffb89e95ea1909275
Suyeon Kang <tndusrkd92@gmail.com>
2020-06-23 17:23:07 +0900

    Convert timestamp to local time of sunrise and sunset

--- app.js
+++ app.js
@@ -5,33 +5,47 @@ const DOMstring = {
     tempDegree:  document.querySelector('.temperature__degree'),
     timezone: document.querySelector('.location__timezone'),
     icon: document.querySelector('.icon'),
+    wind: document.querySelector('.weather-info__wind'),
+    sunrise: document.querySelector('.weather-info__sunrise'),
+    sunset: document.querySelector('.weather-info__sunset'),
+    humidity: document.querySelector('.weather-info__humidity'),
 };
 
 class Weather {
+
+    getLocalTime(timestamp) {
+        let sec = timestamp;
+        let date = new Date(sec * 1000);
+        let timestr = date.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+        return timestr;
+    }
+
     async getData(lat, lon) {
-            const api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=941bf83d26c6b4087d00fe58d945f23e`;
             try {
+                const api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=941bf83d26c6b4087d00fe58d945f23e`;
                 let res = await (await fetch(api)).json();
                 this.location = `${res.name}, ${res.sys.country }`;
                 this.description = res.weather[0].description;
-                //this.icon = `http://openweathermap.org/img/wn/${res.weather[0].icon}.png`;
-                const icon = `http://openweathermap.org/img/wn/${res.weather[0].icon}@2x.png`;
+                this.icon = `http://openweathermap.org/img/wn/${res.weather[0].icon}@2x.png`;
                 this.degree = res.main.temp;
+                this.wind = res.wind.speed;
+                this.humidity = res.main.humidity;
+                this.sunrise = this.getLocalTime(res.sys.sunrise);
+                this.sunset = this.getLocalTime(res.sys.sunset);
                 console.log(res);
-                
-                // Render Data
-                DOMstring.tempDegree.textContent = this.degree;
-                DOMstring.tempDescription.textContent = this.description;
-                DOMstring.timezone.textContent = this.location;
-                DOMstring.icon.src = icon;
+            
             } catch (error) {
                 console.log(error);
             }
     }
-};
 
+    renderWeather() {
+        console.log(this.wind);
+    } 
+}; 
 
-
+const a = new Weather();
+a.renderWeather();
 
 /* Event Handler */
 window.addEventListener('load', () => {
@@ -39,17 +53,14 @@ window.addEventListener('load', () => {
     if (navigator.geolocation) {
         const weather = new Weather();
 
-        navigator.geolocation.getCurrentPosition(position => {
+        navigator.geolocation.getCurrentPosition(position =>  {
             const lon = position.coords.longitude;
             const lat = position.coords.latitude;
 
             // Get data from API
             weather.getData(lat,lon);
-
-            // Render current weather
-            // DOMstring.tempDegree.textContent = weather.description
         });
     }
-});
+}); 
 
 
