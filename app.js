0626b4595cbe50ea1a0404118e6af8dddf23f787
Suyeon Kang <tndusrkd92@gmail.com>
2020-06-23 16:00:06 +0900

    Change style.css

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 const DOMstring = {
     tempDescription: document.querySelector('.temperature__description'),
     tempDegree:  document.querySelector('.temperature__degree'),
-    timezone: document.querySelector('.location__timezone')
+    timezone: document.querySelector('.location__timezone'),
+    icon: document.querySelector('.icon'),
 };
 
 class Weather {
@@ -11,16 +12,25 @@ class Weather {
             const api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=941bf83d26c6b4087d00fe58d945f23e`;
             try {
                 let res = await (await fetch(api)).json();
-                this.location = `${res.name}/${res.sys.country }`;
+                this.location = `${res.name}, ${res.sys.country }`;
                 this.description = res.weather[0].description;
-                this.icon = res.weather[0].icon;
+                //this.icon = `http://openweathermap.org/img/wn/${res.weather[0].icon}.png`;
+                const icon = `http://openweathermap.org/img/wn/${res.weather[0].icon}@2x.png`;
                 this.degree = res.main.temp;
                 console.log(res);
+                
+                // Render Data
+                DOMstring.tempDegree.textContent = this.degree;
+                DOMstring.tempDescription.textContent = this.description;
+                DOMstring.timezone.textContent = this.location;
+                DOMstring.icon.src = icon;
             } catch (error) {
                 console.log(error);
             }
     }
 };
+
+
 
 
 /* Event Handler */
@@ -29,7 +39,7 @@ window.addEventListener('load', () => {
     if (navigator.geolocation) {
         const weather = new Weather();
 
-        navigator.geolocation.getCurrentPosition(position =>{
+        navigator.geolocation.getCurrentPosition(position => {
             const lon = position.coords.longitude;
             const lat = position.coords.latitude;
 
@@ -41,4 +51,5 @@ window.addEventListener('load', () => {
         });
     }
 });
+
 
